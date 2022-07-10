16ce459a1ba9e011e13fc60bc24d340eb368e25b
cintia braulino <cintiabraulino@gamil.com>
2022-07-10 12:05:16 -0300

    organizing folders for project pattern

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import {theme} from "./styles/theme"
 
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
