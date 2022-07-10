59863a61a9dfff64cfc94c415e589a30086135d3
cintia braulino <cintiabraulino@gamil.com>
2022-07-10 12:05:16 -0300

    organizing folders for project pattern

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import { Home } from './pages'
+import { Router } from "./router";
 
 export default function App() {
+
   return (
-    <Router>
-      <Navbar/>
-      <Routes>
-      <Route path="/" element={<Home />}/>
-      </Routes> 
-      <Footer/> 
-    </Router>
+  <>
+    <Navbar/>
+    <Router/>
+    <Footer/> 
+  </>
   );
+
 }
---
+++ src/router/index.jsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from '../pages'
+
+export function Router() {
+    return(
+        <Routes>
+        <Route path="/" element={<Home />}/>
+        </Routes> 
+    )
+}
