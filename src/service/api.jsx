37a841227fc41f161351fa3738b1c69490eb1cb1
heitordiasif <86968135+heitordiasif@users.noreply.github.com>
2022-07-13 18:12:49 -0300

    finalizing process with post and registration

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'process.env.REACT_APP_API'
+    baseURL: process.env.REACT_APP_API
 });
 
 export default api; 
--- src/service/userRegister.jsx
+++ src/service/userRegister.jsx
@@ -1,6 +1,6 @@
 import api from "./api";
 
-export const createUser = async (name, cpfCnpj, email, password, dateNasc) => {
-    const users = await api.post("/user", {name, cpfCnpj, email, password, dateNasc});
+export const createUser = async (name, cpfCnpj, email, password, dateNasc, typeUser=false) => {
+    const users = await api.post("/user", {name, cpfCnpj, email, password, dateNasc, typeUser});
     return users.data;
 };
