c0999342ce4f4a4482b897aca59855b051f3dea6
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-07-15 13:59:03 -0300

    adding new file services

---
+++ src/services/api.jsx
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const api = axios.create({baseURL:process.env.REACT_APP_API});
+
+export default api;
---
+++ src/services/foodService.jsx
@@ -0,0 +1,6 @@
+import api from './api';
+
+export const foodAll = async () => {
+  const food = await api.get('/food');
+  return food.data;
+};
