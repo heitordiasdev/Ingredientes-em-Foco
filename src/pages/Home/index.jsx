9318e6d02fe05fe7b446b123bde0feee48c54ce0
Cintia Braulino <cintiabraulino@gmail.com>
2022-07-16 20:58:44 -0300

    ajustes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import Header from './../../components/SideMenu/Header'
-import MiddleSection from './../../components/SideMenu/MiddleSection'
+import Header from '../../components/SideMenu/Header'
+import MiddleSection from '../../components/SideMenu/MiddleSection'
 
 export default class App extends Component {
   render() {
