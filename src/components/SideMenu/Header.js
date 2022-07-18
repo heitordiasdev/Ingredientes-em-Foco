02f960d9f236ceece0057dcf254585a3ced3fac9
emillymar <emillygmar.silva@gmail.com>
2022-07-18 18:56:37 -0300

    add pointer SideMenu

--- src/components/SideMenu/Header.js
+++ src/components/SideMenu/Header.js
@@ -7,11 +7,11 @@ export default class Header extends Component {
     render() {
         return (
             <List style={{display: 'flex', listStyle: 'none', backgroundColor: '#52691F', color: 'white', height: 50, justifyContent: 'flex-end'}}>
-                <ListItem style={{width: 'auto', marginRight: 40}}><Link activeClass="active" to="inicio" spy={true} smooth={true}>ÍNICIO</Link></ListItem>
-                <ListItem style={{width: 'auto', marginRight: 40}}><Link  to="sobre" spy={true} smooth={true}>QUEM SOMOS</Link></ListItem>
-                <ListItem style={{width: 'auto', marginRight: 40}}><Link  to="alergia" spy={true} smooth={true}>ALERGIA ALIMENTAR</Link></ListItem>
-                <ListItem style={{width: 'auto', marginRight: 40}}><Link  to="principais" spy={true} smooth={true}>PRINCIPAIS ALÉRGENOS</Link></ListItem>
-                <ListItem style={{width: 'auto', marginRight: 40}}><Link  to="produtos" spy={true} smooth={true}>PRODUTOS</Link></ListItem>
+                <ListItem style={{width: 'auto', marginRight: 40, cursor: 'pointer'}}><Link activeClass="active" to="inicio" spy={true} smooth={true}>ÍNICIO</Link></ListItem>
+                <ListItem style={{width: 'auto', marginRight: 40,  cursor: 'pointer'}}><Link  to="sobre" spy={true} smooth={true}>QUEM SOMOS</Link></ListItem>
+                <ListItem style={{width: 'auto', marginRight: 40,  cursor: 'pointer'}}><Link  to="alergia" spy={true} smooth={true}>ALERGIA ALIMENTAR</Link></ListItem>
+                <ListItem style={{width: 'auto', marginRight: 40,  cursor: 'pointer'}}><Link  to="principais" spy={true} smooth={true}>PRINCIPAIS ALÉRGENOS</Link></ListItem>
+                <ListItem style={{width: 'auto', marginRight: 40,  cursor: 'pointer'}}><Link  to="produtos" spy={true} smooth={true}>PRODUTOS</Link></ListItem>
             </List>
         )
     }
