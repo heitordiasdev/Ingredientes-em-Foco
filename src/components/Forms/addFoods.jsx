1b517cf32a51a1829fd5643441aa43907193b434
Igor Silva <igorpas93@gmail.com>
2022-07-20 01:19:07 -0300

    Reload page

--- src/components/Forms/addFoods.jsx
+++ src/components/Forms/addFoods.jsx
@@ -22,6 +22,9 @@ function DialogNewProd(props) {
   const [newIngredients, setIngredients] = useState('')
   const [newIngredientsCheck, setIngredientsCheck] = useState('')
 
+  useEffect(() => {
+  }, [error, newName, newManufacture, newIngredients, newIngredientsCheck, rows ]);
+
   const addRow = (index) => {
     setRows([...rows, { name:'', quantity:'', vd:''}])
   }
