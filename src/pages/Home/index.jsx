77c763e08bb030cbab66462bc2e952d12cab922a
emillymar <emillygmar.silva@gmail.com>
2022-07-20 15:15:43 -0300

    Home 2.0

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Header from '../../components/SideMenu/Header'
 import MiddleSection from '../../components/SideMenu/MiddleSection'
+import Scroll from '../../components/Scroll/index'
 
 export default class App extends Component {
   render() {
     return (
       <>
        <Header/>
+       <Scroll showBelow={250}/>
        <MiddleSection/>
       </>
     );
