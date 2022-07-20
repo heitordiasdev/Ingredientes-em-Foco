c569175fbead4e9c29f32f59a5390e0b2f837088
Igor Silva <igorpas93@gmail.com>
2022-07-20 01:19:07 -0300

    Reload page

--- src/pages/Provider/index.jsx
+++ src/pages/Provider/index.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import ListFoods from '../../components/List/ListFoods';
-import LoadFoods from '../../services/provider';
-// import { foodAll }from '../../services/foodService';
+// import LoadFoods from '../../services/provider';
+import { foodAll }from '../../services/foodService';
 
 function Provider() {
   const [foods, setFoods] = useState({});
   const [ loading, setLoading ] = useState(true);
-  const { getFoods } = LoadFoods.List();
+  // const { getFoods } = LoadFoods.List();
   
   useEffect(() => {
-        (async () => {
-          const getFoodsFromApi = await getFoods();
-
-          setFoods(getFoodsFromApi);
-          setLoading(false);
-        })();
+    (async () => {
+      const getFoodsFromApi = await foodAll();
+      setFoods(getFoodsFromApi);
+      setLoading(false)
+    })();
   }, []);
-    return (
-        <>
-          <ListFoods foods={foods} loading={loading} />
-        </>
-    )   
+
+    const refresh = async() => {
+      setLoading(true)
+      const getFoodsFromApi = await foodAll();
+      setFoods(getFoodsFromApi);
+      setLoading(false)
+      console.log('Refresh', loading)
+    }
+  return (
+    <>
+      <ListFoods foods={foods} loading={loading} setLoading={refresh} />
+    </>
+  )   
 };
 export default Provider
