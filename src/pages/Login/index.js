289aff21943f948ea3b84253f158a835fe1d979b
heitordiasif <86968135+heitordiasif@users.noreply.github.com>
2022-07-20 23:13:43 -0300

    some updates

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FormLogin from '../../components/FormLogin';
+import HeaderGlobal from '../../components/HeaderGlobal';
 
 
 export default function Login() {
     return(
         
         <div>
+        <HeaderGlobal></HeaderGlobal>
         <FormLogin></FormLogin>
         </div>
         
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FormRegister from '../../components/FormRegister';
+import HeaderGlobal from '../../components/HeaderGlobal';
 
 
 export default function Register() {
     return(
         
         <div>
+        <HeaderGlobal></HeaderGlobal>
         <FormRegister></FormRegister>
         </div>
         
